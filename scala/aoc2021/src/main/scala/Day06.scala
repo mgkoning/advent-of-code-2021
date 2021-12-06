a49d9190e89e013d0bedf828992223fbcf923713
Michiel Koning <github@michiel.ion3.nl>
2021-12-06 17:54:04 +0100

    Day 06: Lanternfish

--- scala/aoc2021/src/main/scala/Day06.scala
+++ scala/aoc2021/src/main/scala/Day06.scala
@@ -1,8 +1,39 @@
 import scala.io.Source
+import scala.annotation.tailrec
 
 object Day06 extends PuzzleSolution {
-  def title = "<?>"
+  def title = "Lanternfish"
 
   def solve(input: Source) =
-    println("Part 1")
+    val ages = readAges(input.toSeq)
+    println("Part 1:")
+    println(lanternfishAfterDays(80, ages))
+    println("Part 2:")
+    println(lanternfishAfterDays(256, ages))
+
+  def lanternfishAfterDays(days: Int, startAges: Seq[Int]): Long =
+    val cache = getBirthTimeCache(days)
+    startAges.map(age => cache.get(age - 8).get).sum
+
+  // to prevent calculating the same values over and over, we work 'backwards' from the maximum age:
+  // determine how many fish would be spawned if a lanternfish is born at a certain time. We will
+  // eventually also visit its parent, and we'll know how many spawns the parent will have, plus
+  // how many spawn their spawn will have. We accumulate it to -10 because the fish we start with
+  // were born in the past.
+  def getBirthTimeCache(maxAge: Int): Map[Int, Long] =
+    @tailrec def buildCache(age: Int, accumulated: Map[Int, Long]): Map[Int, Long] =
+      age match
+        case i if i < -10 => accumulated
+        case i =>
+          val firstSpawn = i + 9
+          val spawns = Seq.iterate(firstSpawn, maxAge / 6 + 1)(_ + 7).takeWhile(_ <= maxAge)
+          val allSpawns = spawns.map(age => accumulated.get(age).get).sum
+          buildCache(age - 1, accumulated.updated(i, 1 + allSpawns))
+
+    val initialMap = (0 to 7).map(maxAge - _).map(_ -> 1L).toMap
+    buildCache(maxAge - 8, initialMap)
+
+  val toIgnore = Set(',', '\n', '\r')
+  def readAges(chars: Seq[Char]): List[Int] =
+    chars.filter(!toIgnore.contains(_)).map(_ - '0').toList
 }
---
+++ scala/aoc2021/src/test/scala/Day06Test.scala
@@ -0,0 +1,14 @@
+import org.junit.Test
+import org.junit.Assert.*
+import Day06.*
+
+class Day06Test {
+  val input = "3,4,3,1,2"
+
+  @Test
+  def part1 =
+    val ages = readAges(input.map(identity))
+    assertEquals(26, lanternfishAfterDays(18, ages))
+    assertEquals(5934, lanternfishAfterDays(80, ages))
+    assertEquals(26984457539L, lanternfishAfterDays(256, ages))
+}
