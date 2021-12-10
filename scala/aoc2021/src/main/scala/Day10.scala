044c8d9ca5780f2b7f7435c94a338011722e8a8b
Michiel Koning <github@michiel.ion3.nl>
2021-12-10 08:06:46 +0100

    Day 10: Syntax Scoring (with less parens)

--- scala/aoc2021/src/main/scala/Day10.scala
+++ scala/aoc2021/src/main/scala/Day10.scala
@@ -3,20 +3,18 @@ object Day10 extends PuzzleSolution {
   def title = "Syntax Scoring"
 
   def solve(input: scala.io.Source) =
-    val lines = input.getLines.toSeq
+    val lines = input.getLines.toList
     println("Part 1:")
-    val parsedLines = lines.map(parseLine).toList
-    val corrupt = parsedLines.map(scoreCorrupt).sum
+    val parsedLines = lines map parseLine.andThen(score)
+    val corrupt = parsedLines.flatMap(_.left.toSeq).sum
     println(corrupt)
     println("Part 2:")
-    val incompleteScores = parsedLines.map(scoreIncomplete).flatMap(_.toSeq).sorted
+    val incompleteScores = parsedLines.flatMap(_.toSeq).sorted
     println(incompleteScores.drop(incompleteScores.size / 2).head)
 
-  def scoreCorrupt(e: Either[Char, List[Char]]): Int =
-    e.fold(scoreUnmatched, Function.const(0))
-
-  def scoreIncomplete(e: Either[Char, List[Char]]): Either[Char, Long] =
-    e.map(cs => cs.foldLeft(0L)((s, c) => s * 5 + scoreMissing(c)))
+  def score(e: Either[Char, List[Char]]): Either[Int, Long] =
+    e.map(_.map(scoreMissing).foldLeft(0L)((s, c) => s * 5 + c))
+      .left.map(scoreUnmatched)
 
   // parses the given line and returns Left with an incorrect closing char if found, or Right with
   // the missing closing chars at the end of the line.
@@ -24,9 +22,9 @@ object Day10 extends PuzzleSolution {
     @tailrec def parseLineInner(stack: List[Char], remaining: List[Char]): Either[Char, List[Char]] =
       remaining match
         case Nil => Right(stack.map(matchFor))
-        case (x :: xs) if openChars.contains(x) => parseLineInner(x :: stack, xs)
-        case (x :: xs) =>
-            val (top :: rest) = stack
+        case x :: xs if openChars contains x => parseLineInner(x :: stack, xs)
+        case x :: xs =>
+            val top :: rest = stack
             if x == matchFor(top)
               then parseLineInner(rest, xs)
               else Left(x)
