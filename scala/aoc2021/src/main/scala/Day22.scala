66012d855d704176923b5e74d4c77ef88a52bba5
Michiel Koning <github@michiel.ion3.nl>
2021-12-22 16:27:42 +0100

    Day 22: Reactor Reboot (clean up and comments)

--- scala/aoc2021/src/main/scala/Day22.scala
+++ scala/aoc2021/src/main/scala/Day22.scala
@@ -39,31 +39,42 @@ object Day22 extends PuzzleSolution:
         case Off => cubes.diff(stepCubes))
 
   def runSteps(steps: List[RebootStep]): List[Cuboid] =
+    // for every step: determine which parts of the currently 'On' cuboids do not overlap
+    // with the step's area. Retain those parts for the next iteration, and add the current
+    // area only if the state is `On`.
     steps.foldLeft(List.empty[Cuboid])((onCubes, step) =>
       val RebootStep(state, area) = step
       val piecesOutside = onCubes.flatMap(shatter(area))
       if state == On then area::piecesOutside else piecesOutside)
 
+  // Breaks `current` into pieces that do not overlap with `previous`.
   def shatter(previous: Cuboid)(current: Cuboid): Seq[Cuboid] =
     if whollyWithin(previous)(current) then Seq.empty
     else
       overlaps(previous)(current) match
         case None => Seq(current)
         case Some(overlap) =>
-          def pairs(selector: Coord3 => Int) =
-            Seq((selector(current.from), selector(overlap.from) - 1),
+          // Determine the pairs of coordinates by axis to create the pieces outside
+          // of the overlap. Note that the overlap cuboid has inclusive boundaries.
+          def pairs(selector: Coord3 => Int) = Seq(
+              (selector(current.from), selector(overlap.from) - 1),
               (selector(overlap.from), selector(overlap.to)),
               (selector(overlap.to) + 1, selector(current.to)))
-          val subcubes = for (x0, x1) <- pairs(_.x); (y0, y1) <- pairs(_.y); (z0, z1) <- pairs(_.z)
+          val subcubes = for (x0, x1) <- pairs(_.x);
+                             (y0, y1) <- pairs(_.y);
+                             (z0, z1) <- pairs(_.z)
                          yield Cuboid(Coord3(x0, y0, z0), Coord3(x1, y1, z1))
-          subcubes
+          subcubes // remove cubes that are zero-sized as well as the overlap.
             .filter(0 < Cuboid.size(_))
             .filter(!whollyWithin(overlap)(_))
 
+  // Determines the overlap between `one` and `other`.
   def overlaps(one: Cuboid)(other: Cuboid): Option[Cuboid] =
+    val Cuboid(from0, to0) = one
+    val Cuboid(from1, to1) = other
     val overlap = Cuboid(
-      Coord3(math.max(other.from.x, one.from.x), math.max(other.from.y, one.from.y), math.max(other.from.z, one.from.z)),
-      Coord3(math.min(other.to.x, one.to.x), math.min(other.to.y, one.to.y), math.min(other.to.z, one.to.z))
+      Coord3(math.max(from0.x, from1.x), math.max(from0.y, from1.y), math.max(from0.z, from1.z)),
+      Coord3(math.min(to0.x, to1.x), math.min(to0.y, to1.y), math.min(to0.z, to1.z))
     )
     if 0 < Cuboid.size(overlap) then Some(overlap) else None
 
@@ -75,10 +86,9 @@ object Day22 extends PuzzleSolution:
 
   def readRebootSteps(lines: Iterator[String]): List[RebootStep] =
     lines
-      .flatMap(l =>
-        l match
-          case s"$state x=$x0..$x1,y=$y0..$y1,z=$z0..$z1" =>
-            val area = Cuboid(Coord3.ofStrings(x0, y0, z0), Coord3.ofStrings(x1, y1, z1))
-            Some(RebootStep(if state == "on" then On else Off, area))
-          case _ => None)
+      .flatMap(line => line match
+        case s"$state x=$x0..$x1,y=$y0..$y1,z=$z0..$z1" =>
+          val area = Cuboid(Coord3.ofStrings(x0, y0, z0), Coord3.ofStrings(x1, y1, z1))
+          Some(RebootStep(if state == "on" then On else Off, area))
+        case _ => None)
       .toList
