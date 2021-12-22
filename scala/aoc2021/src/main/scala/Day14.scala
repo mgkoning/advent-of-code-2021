ecb61533c39bfe71d5a78ad4cb7c6011eaeed4d7
Michiel Koning <github@michiel.ion3.nl>
2021-12-22 21:55:40 +0100

    Day 14: Extended Polymerization (with a memoized variant)

--- scala/aoc2021/src/main/scala/Day14.scala
+++ scala/aoc2021/src/main/scala/Day14.scala
@@ -15,24 +15,56 @@ object Day14 extends PuzzleSolution:
     val initialPolymerPairCounts = polymer.zip(polymer.drop(1))
       .groupBy(identity).mapValues(_.size.toLong).toList
     val after40 = LazyList.iterate(initialPolymerPairCounts)(stepPairwise(rules)).drop(40).head
-    val charCounts = after40.groupMap(_._1._1)(_._2)
+    println(answer(lastChar, after40))
+    println("Part 2 memoized:")
+    val productionResult = runMemoized(rules)(polymer, 40)
+    println(answer(lastChar, productionResult))
+
+  def answer(lastChar: Char, pairCounts: List[PolymerPairCount]): Long =
+    val charCounts = pairCounts.groupMap(_._1._1)(_._2)
       .map((c, v) => if c == lastChar then v.sum + 1 else v.sum)
-    println(charCounts.max - charCounts.min)
+    charCounts.max - charCounts.min
 
   def resultAfter(rules: Rules, steps: Int)(polymer: List[Char]): Map[Char, Int] =
     val afterSteps = LazyList.iterate(polymer)(step(rules)).drop(steps).head
     afterSteps.groupBy(identity).mapValues(_.size).toMap
 
   def stepPairwise(rules: Rules)(pairs: List[PolymerPairCount]): List[PolymerPairCount] =
-    def applyRule(pair: PolymerPair): List[PolymerPair] =
-      val (left, right) = pair
-      rules.get(pair).map(b => List((left, b), (b, right))).toList.flatMap(identity)
-
-    val newPairs = pairs.flatMap((pair, count) => applyRule(pair).map(newPair => (newPair, count)))
+    val applyRuleFor = applyRule(rules)
+    val newPairs = pairs.flatMap((pair, count) => applyRuleFor(pair).map(newPair => (newPair, count)))
     newPairs.foldLeft(Map.empty[PolymerPair, Long])(
       (m, next) => 
         val (pair, count) = next
         m.updatedWith(pair)(v => v.map(_ + count).orElse(Some(count)))).toList
+
+  def applyRule(rules: Rules)(pair: PolymerPair): List[PolymerPair] =
+    val (left, right) = pair
+    rules.get(pair).map(b => List((left, b), (b, right))).getOrElse(List(pair))
+
+  type PairProductionCache = Map[(PolymerPair, Int), List[PolymerPairCount]]
+  def runMemoized(rules: Rules)(polymer: List[Char], steps: Int): List[PolymerPairCount] =
+    val applyRuleFor = applyRule(rules)
+
+    def foldPairCount(pairs: List[PolymerPair], steps: Int, cache: PairProductionCache): (List[PolymerPairCount], PairProductionCache) =
+      val startValue = (List.empty[PolymerPairCount], cache)
+      pairs.foldLeft(startValue)((acc, pair) =>
+        val (result, cache) = acc
+        val (pairResults, updatedCache) = pairCount(pair, steps, cache)
+        (result.concat(pairResults).groupBy(_._1).mapValues(_.map(_._2).sum).toList, updatedCache))
+
+    def pairCount(pair: PolymerPair, steps: Int, cache: PairProductionCache): (List[PolymerPairCount], PairProductionCache) =
+      if steps <= 0 then
+        val pairCount = List((pair, 1L))
+        (pairCount, cache.updated((pair, steps), pairCount))
+      else cache.get((pair, steps)) match
+        case Some(result) => (result, cache)
+        case None =>
+          val (result, updatedCache) = foldPairCount(applyRuleFor(pair), steps - 1, cache)
+          (result, updatedCache.updated((pair, steps), result))
+
+    val emptyCache = Map.empty[(PolymerPair, Int), List[PolymerPairCount]]
+    val (total, _) = foldPairCount(polymer.zip(polymer.drop(1)), steps, emptyCache)
+    total
 
   def step(rules: Rules)(polymer: List[Char]): List[Char] =
     def polymerize(remaining: List[Char], soFar: List[Char]): List[Char] =
